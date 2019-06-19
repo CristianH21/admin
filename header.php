21dde518ec26917c6e517568287bfde752f6017f
CristianH21 <crishhtona21@gmail.com>
2019-06-19 09:04:35 -0500

    version 1.0.0

---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Twitter -->
+    <meta name="twitter:site" content="@themepixels">
+    <meta name="twitter:creator" content="@themepixels">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Amanda">
+    <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="twitter:image" content="http://themepixels.me/amanda/img/amanda-social.png">
+
+    <!-- Facebook -->
+    <meta property="og:url" content="http://themepixels.me/amanda">
+    <meta property="og:title" content="Bracket">
+    <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
+
+    <meta property="og:image" content="http://themepixels.me/amanda/img/amanda-social.png">
+    <meta property="og:image:secure_url" content="http://themepixels.me/amanda/img/amanda-social.png">
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="600">
+
+    <!-- Meta -->
+    <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
+    <meta name="author" content="ThemePixels">
+
+
+    <title>Amanda Responsive Bootstrap 4 Admin Template</title>
+
+    <!-- vendor css -->
+    <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet">
+    <link href="lib/Ionicons/css/ionicons.css" rel="stylesheet">
+    <link href="lib/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet">
+    <link href="lib/jquery-toggles/toggles-full.css" rel="stylesheet">
+    <link href="lib/rickshaw/rickshaw.min.css" rel="stylesheet">
+
+    <!-- Amanda CSS -->
+    <link rel="stylesheet" href="css/amanda.css">
+  </head>
+
+  <body>
+
+    <div class="am-header">
+        <div class="am-header-left">
+            <a id="naviconLeft" href="" class="am-navicon d-none d-lg-flex"><i class="icon ion-navicon-round"></i></a>
+            <a id="naviconLeftMobile" href="" class="am-navicon d-lg-none"><i class="icon ion-navicon-round"></i></a>
+            <a href="index.php" class="am-logo"><b>eHome</b>media</a>
+        </div><!-- am-header-left -->
+
+
+        <div class="am-header-right">
+            <a href="#" title="View Website">
+                <i class="icon ion-cloud tx-24"></i>
+            </a>
+            <div class="dropdown dropdown-profile">
+                <a href="" class="nav-link nav-link-profile" data-toggle="dropdown">
+                    <img src="../img/img3.jpg" class="wd-32 rounded-circle" alt="">
+                    <span class="logged-name"><span class="hidden-xs-down">Cristian Tonathiu Hernandez Heredia</span> <i class="fa fa-angle-down mg-l-3"></i></span>
+                </a>
+                <div class="dropdown-menu wd-200">
+                    <ul class="list-unstyled user-profile-nav">
+                        <li><a href=""><i class="icon ion-ios-person-outline"></i> Edit Profile</a></li>
+                        <li><a href=""><i class="icon ion-ios-gear-outline"></i> Settings</a></li>
+                        <li><a href=""><i class="icon ion-ios-folder-outline"></i> Log</a></li>
+                        <li><a href=""><i class="icon ion-power"></i> Sign Out</a></li>
+                    </ul>
+                </div><!-- dropdown-menu -->
+            </div><!-- dropdown-profile -->
+        </div><!-- am-header-right -->
+    </div><!-- am-header -->
---
+++ service.php
@@ -0,0 +1,97 @@
+<?php
+  require 'header.php';
+  require 'sidebar.php';
+?>
+
+
+    <div class="am-mainpanel">
+      <div class="am-pagetitle">
+        <h5 class="am-title">Service</h5>
+      </div><!-- am-pagetitle -->
+
+      <div class="am-pagebody">
+            <div class="card pd-20 pd-sm-40">
+                <div class="row">
+                    <div class="col-lg-11">
+                        <h6 class="card-body-title">Services</h6>
+                        <p class="mg-b-20 mg-sm-b-30">Help customers know what you offer.</p>
+                    </div>
+                    <div class="col-lg-1">
+                        <button class="btn btn-primary">Create</button>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table class="table mg-b-0">
+                        <thead>
+                            <tr>
+                            <th>
+                                <label class="ckbox mg-b-0">
+                                <input type="checkbox"><span></span>
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>User</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <label class="ckbox mg-b-0">
+                                    <input type="checkbox"><span></span>
+                                    </label>
+                                </td>
+                                <td>Factura Electrónica</td>
+                                <td>Admin</td>
+                                <td>2019/07/21</td>
+                                <td>
+                                    <ul>
+                                        <li>Edit</li>
+                                        <li>Delete</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label class="ckbox mg-b-0">
+                                    <input type="checkbox"><span></span>
+                                    </label>
+                                </td>
+                                <td>Nomina Electrónico</td>
+                                <td>Admin</td>
+                                <td>2019/07/21</td>
+                                <td>
+                                    <ul>
+                                        <li>Edit</li>
+                                        <li>Delete</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label class="ckbox mg-b-0">
+                                    <input type="checkbox"><span></span>
+                                    </label>
+                                </td>
+                                <td>Resplado de Información</td>
+                                <td>Admin</td>
+                                <td>2019/07/21</td>
+                                <td>
+                                    <ul>
+                                        <li>Edit</li>
+                                        <li>Delete</li>
+                                    </ul>
+                                </td>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div><!-- responsive table -->
+            </div><!-- card -->
+      </div><!-- am-pagebody -->
+    </div><!-- am-mainpanel -->
+
+<?php 
+  require 'footer.php';
+?>
